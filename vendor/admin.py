0d63705ca1d2047d37ce291e01af20356da469e8
Gajjelli Ramesh <gajjelli.ramesh.com>
2024-08-07 09:54:37 +0530

    reset password

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
 from vendor.models import Vendor
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(Vendor)
+class CustomVendorAdmin(UserAdmin):
+    list_display = ("user", "vendor_name", "is_approved", "created_at")
+    ordering = ("-user",)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Vendor, CustomVendorAdmin)
